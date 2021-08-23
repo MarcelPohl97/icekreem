8b24527cdadcb4db924fbfead2428e37760a1587
Marcel <45712008+MarcelPohl97@users.noreply.github.com>
2021-08-23 14:08:49 +0200

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "icekreem",
+  },
+  plugins: [
+    {
+      resolve: "gatsby-source-contentful",
+      options: {
+        accessToken: "kDHyg0TTO6eT2IEdLOvsTy1uuT0fIQyaSnx0OzrIZwQ",
+        spaceId: "8ae5xhoiw45y",
+      },
+    },
+    "gatsby-plugin-gatsby-cloud",
+  ],
+};
